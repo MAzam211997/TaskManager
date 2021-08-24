294072d4d3c68a60b3cf1d46a579af3c2338c06c
MAzam211997 <mazam19971@gmail.com>
2021-08-24 23:43:10 +0500

    Add project files.

---
+++ TaskManager.Entities/AuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Entities
+{
+    public class AuditableEntity
+    {
+        public DateTime? CreatedDate{ get; set; }
+        public DateTime? ModifiedDate{ get; set; }
+        public DateTime? DeletedDate{ get; set; }
+        public int? CreatedBy { get; set; }
+        public int? ModifiedBy { get; set; }
+        public int? DeletedBy { get; set; }
+    }
+}
